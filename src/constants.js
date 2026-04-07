d1030c8d1a9a4de449da7db4d543d3c7eafd2337
taniya-gupta-95x <taniyagupta432@gmail.com>
2026-04-07 20:35:25 +0530

    Fixed image path error

--- src/constants.js
+++ src/constants.js
@@ -35,7 +35,7 @@ import MyPortfolioLogo from './assets/work_logo/MyPortfolio.png';
 import SalesDashboardLogo from './assets/work_logo/Sales Dashboard.png';
 import WeatherAppLogo from './assets/work_logo/WeatherApp.png';
 import SnakeGameLogo from './assets/work_logo/Snake Game.png';
-import ImageSearchLogo from './assets/work_logo/Image_Search.png';
+import ImageSearchLogo from './assets/work_logo/image_search.png';
 import removebgLogo from './assets/work_logo/Remove_bg.png';
 
 
